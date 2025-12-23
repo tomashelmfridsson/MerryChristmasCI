1d22014ba8dd93bd4dced2c27e0adeea4863f53a
tamaraalsaad <tamaraalsaad@yahoo.com>
2025-12-23 16:36:05 +0100

    Tamara

--- src/main/java/GodJul.java
+++ src/main/java/GodJul.java
@@ -1,3 +1,9 @@
 public class GodJul {
+    public static void main(String[] args) {
 
+        TomteNisse tomteNisse = new TomteNisse("Nisse", 120);
+        System.out.println(tomteNisse.getNamn()+"är"+tomteNisse.getYears()+"år gammal");
+
+
+    }
 }
