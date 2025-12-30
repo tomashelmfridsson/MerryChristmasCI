73a8aedede92f1c11171123bda6c53ea93a8019a
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-12-30 10:16:25 +0100

    uppdaterade testerna

--- src/main/java/nisseTest.java
+++ src/main/java/nisseTest.java
@@ -1,6 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class nisseTest {
 
@@ -34,7 +34,3 @@ public class nisseTest {
         assertEquals("Bob", tomas.getName());
     }
 }
-
-
-
- */
