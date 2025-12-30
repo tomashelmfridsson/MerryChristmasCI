dc3c119cb267230cccf35828d4f396802cb45627
LegendBerrie <lovisablomster@gmail.com>
2025-12-30 11:17:45 +0100

    Fixade så att allt är på engelska

--- src/main/java/GodJul.java
+++ src/main/java/GodJul.java
@@ -1,9 +1,16 @@
 public class GodJul {
+
     public static void main(String[] args) {
-        TomteNisse tomas = new TomteNisse("Tomas", 50);
-        System.out.println("Den här Nissen heter "+tomas.getName());
-        System.out.println(tomas.getName() + " är " +tomas.getYears());
-        tomas.changeName("Bob");
-        System.out.println("Nu heter Nissen "+tomas.getName());
+
+
+        TomteNisse nisse1 = new TomteNisse("Nisse", 574);
+        TomteNisse nisse2 = new TomteNisse("Tomas", 420);
+        TomteNisse nisse3 = new TomteNisse("Tamara", 175);
+
+        System.out.println("Dessa nissar har tomten:");
+        System.out.println(nisse1.getName() + ", " + nisse1.getAge() + " år");
+        System.out.println(nisse2.getName() + ", " + nisse2.getAge() + " år");
+        System.out.println(nisse3.getName() + ", " + nisse3.getAge() + " år");
+
     }
 }
