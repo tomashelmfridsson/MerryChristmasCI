8abc81dc380f9b14138883686899b014737dfbd9
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-12-30 09:53:51 +0100

    change to 150 år

--- src/main/java/GodJul.java
+++ src/main/java/GodJul.java
@@ -1,7 +1,7 @@
 public class GodJul {
     public static void main(String[] args) {
 
-        TomteNisse tomteNisse = new TomteNisse("Nisse", 120);
+        TomteNisse tomteNisse = new TomteNisse("Nisse", 150);
         System.out.println(tomteNisse.getNamn()+ "är" + tomteNisse.getYears()+"år gammal");
 
 
